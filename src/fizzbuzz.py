a5b44157de25d51566643214067bed873a086979
Nicolas Landier <nicolas.landier@gmail.com>
2021-10-27 12:27:54 +0200

    First test

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,12 +1,9 @@
 from functools import reduce
 
-def sum(numbers):
-    return reduce(lambda a, b: a+b, numbers)
 
+def fizzbuzz():
+    return None
 
-def calc(numbers):
-    result = sum(numbers)
-    print(f"{numbers} => {result}")
 
 if __name__ == '__main__':
-    calc([0])
+    fizzbuzz()
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,24 +1,12 @@
 import unittest
 
-from src.fizzbuzz import sum
+from src.fizzbuzz import fizzbuzz
 
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_list_int(self):
-        """
-        Test that it can sum a list of integers
-        """
-        data = [1, 2, 3]
-        result = sum(data)
-        self.assertEqual(result, 6)
-
-    def test_list_fraction(self):
-        """
-        Test that it can sum a list of fractions
-        """
-        data = [1/4, 1/4, 2/5]
-        result = sum(data)
-        self.assertEqual(result, 0.9)
+    def test_dummy(self):
+        result = fizzbuzz()
+        self.assertEqual(result, None)
 
     @unittest.SkipTest
     def test_bad_type(self):
