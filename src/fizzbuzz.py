ff12cb33e6d331a38a58b79bbe3e0bab51801667
Nicolas Landier <nicolas.landier@gmail.com>
2021-10-27 14:09:40 +0200

    FizzBuzz code

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,9 +1,21 @@
 from functools import reduce
 
 
-def fizzbuzz():
-    return None
+def fizzbuzz(n):
+    r = ""
+    
+    if n % 3 == 0: 
+        r += "fizz"
+    if n % 5 == 0: 
+        r += "buzz"
+    
+    return r or str(n)
 
+def loop_fizzbuzz(t: int):
+    results = []
+    for i in range(1, t + 1):
+        results.append(fizzbuzz(i))
+    return results
 
 if __name__ == '__main__':
     fizzbuzz()
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,12 +1,52 @@
 import unittest
 
-from src.fizzbuzz import fizzbuzz
+from src.fizzbuzz import fizzbuzz, loop_fizzbuzz
 
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_dummy(self):
-        result = fizzbuzz()
-        self.assertEqual(result, None)
+    # def test_dummy(self):
+    #     result = fizzbuzz()
+    #     self.assertEqual(result, None)
+
+
+    def test_when_1_return_fizz(self):
+        result = fizzbuzz(1)
+        self.assertEqual(result, '1')
+
+    def test_when_3_return_fizz(self):
+        result = fizzbuzz(3)
+        self.assertEqual(result, "fizz")
+
+    def test_when_5_return_buzz(self):
+        result = fizzbuzz(5)
+        self.assertEqual(result, "buzz")
+
+    def test_when_15_return_buzz(self):
+        result = fizzbuzz(15)
+        self.assertEqual(result, "fizzbuzz")
+
+    def test_when_9_return_fizz(self):
+        result = fizzbuzz(9)
+        self.assertEqual(result, "fizz")
+
+    def test_when_10_return_buzz(self):
+        result = fizzbuzz(10)
+        self.assertEqual(result, "buzz")
+
+    def test_when_30_return_buzz(self):
+        result = fizzbuzz(30)
+        self.assertEqual(result, "fizzbuzz")
+
+    def test_loop_returns_5_times(self):
+        expected_list = [
+            '1',
+            '2',
+            'fizz',
+            '4',
+            'buzz'
+        ]
+        result = loop_fizzbuzz(5)
+        self.assertEqual(result, expected_list)
 
     @unittest.SkipTest
     def test_bad_type(self):
